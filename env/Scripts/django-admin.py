9487eb4709a8e49b12c1fa64ca7a0278da9454e1
shbz1690 <msk90@georgetown.edu>
2017-09-07 10:54:23 -0400

    added env

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mskhan2\Documents\CTSI 2018\FABRIC_LINUX_DJANGO\fabricApp\fabricApp\fabricApp\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
